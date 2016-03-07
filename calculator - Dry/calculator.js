da70d86dae68e41abba4e43d209171ec002ba466
rcameron23 <cameronganter@gmail.com>
2016-03-07 13:03:58 -0500

    Added files via upload

--- calculator - Dry/calculator.js
+++ calculator - Dry/calculator.js
@@ -1,26 +1,29 @@
-// Calculator Program
-// 4 Operations addition, subtraction,
-// multiplication, and division
-// You will need a function for each one
-// You need a menu so user can select an
-// operation
-
-// input
-// var choice = prompt("What Operation?", "addition");
 var handleClick = function(event){
 	console.log(window.event);
+
 	var choice = window.event.target.id;
 	var number1 = document.getElementById("firstNum").value;
 	var number2 = document.getElementById("secondNum").value;
 	var parsedNumber1 = parseFloat(number1);
 	var parsedNumber2 = parseFloat(number2);
 	var answer;
-		if(choice == "addition"){
+	var operation;
+
+	if(choice == "addition"){
+		operation = " + ";
 		answer = parsedNumber1 + parsedNumber2;
 	} else if(choice == "subtraction"){
+		operation = " - ";
 		answer = parsedNumber1 - parsedNumber2;
 	} else if(choice == "multiplication"){
+		operation = " * ";
 		answer = parsedNumber1 * parsedNumber2;
 	} else if(choice == "division"){
-		answer = parsedNumber1 / parsedNumber2;}	
-		document.write("The answer is " + answer);}
+		operation = " / ";
+		answer = parsedNumber1 / parsedNumber2;
+	}	
+
+	var answersArea = document.getElementById("answers");
+	answersArea.innerHTML = number1 + operation + number2 + " = " + answer;
+
+}
